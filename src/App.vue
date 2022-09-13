a2b77cd8adb9e86889d743e56c3676ba6f6abe03
ystone17 <ston5519@gmail.com>
2022-09-14 01:37:49 +0900

    ENH: 2강 state 를 이용한 데이터바인딩 문법 ENH: 3강 v-for 를 이용한 html tag 반복 생성 ENH: 4강 v-on 혹은 @ 을 이용한 이벤트 핸들러 작성, method 작성 공간 활용 ENH: 5강 <img> 태그 상대 경로 활용 방법,  <style> 태그와 state 를 이용한 모달창 작성

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,100 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="black-bg" v-if="모달창열렸니">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="모달창열렸니 = false">닫기</button>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a v-for="menu in menus" :key="menu">{{ menu }}</a>
+  </div>
+
+  <div>
+    <img src="./assets/room0.jpg" class="room-img" />
+    <h4 @click="모달창열렸니 = true">{{ products[0] }}</h4>
+    <p>50 만원</p>
+    <button @click="increase(0)">허위매물신고</button>
+    <span>신고수 :{{ 신고수[0] }} </span>
+  </div>
+  <div>
+    <img src="./assets/room1.jpg" class="room-img" />/>
+    <h4>{{ products[1] }}</h4>
+    <p>60 만원</p>
+    <button @click="increase(1)">허위매물신고</button>
+    <span>신고수 :{{ 신고수[1] }} </span>
+  </div>
+  <div>
+    <img src="./assets/room2.jpg" class="room-img" /> />
+    <h4>{{ products[2] }}</h4>
+    <p>70 만원</p>
+    <button @click="increase(2)">허위매물신고</button>
+    <span>신고수 :{{ 신고수[2] }} </span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      모달창열렸니: false,
+      신고수: [0, 0, 0],
+      menus: ["Home", "Shop", "About"],
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+    };
+  },
+  methods: {
+    increase(i) {
+      this.신고수[i]++;
+    },
+  },
+  components: {},
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
