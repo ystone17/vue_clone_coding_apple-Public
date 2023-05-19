89781cb557c127f4689633c392a2138d862578fa
ystone17 <ston5519@gmail.com>
2023-05-19 22:39:51 +0900

    7.5 태스크 관리 애플리케이션의 상태 관리
    7.5.3 새로운 태스크 생성 및 완료 처리
    
    * form 태그 추가
    * store 에 mutation 추가
    * App.vue 에 mutation 실행할 method 추가

--- 7. Vuex를 이용한 데이터플로 설계 및 상태 관리/7.5 태스크 관리 애플리케이션의 상태 관리/temp/src/App.vue
+++ 7. Vuex를 이용한 데이터플로 설계 및 상태 관리/7.5 태스크 관리 애플리케이션의 상태 관리/temp/src/App.vue
@@ -7,14 +7,36 @@
         {{ task.name }}
       </li>
     </ul>
+
+    <form v-on:submit.prevent="addTask">
+      <input type="text" v-model="newTaskName" placeholder="새 태스크">
+    </form>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      newTaskName: ''
+    }
+  }, 
   computed: {
     tasks() {
       return this.$store.state.tasks
+    }
+  },
+  methods: {
+    addTask() {
+      this.$store.commit('addTask', {
+        name: this.newTaskName
+      })
+      this.newTaskName = ''
+    },
+    toggleTaskStatus(task) {
+      this.$store.commit('toggleTaskStatus', {
+        id: task.id
+      })
     }
   }
 }
--- 7. Vuex를 이용한 데이터플로 설계 및 상태 관리/7.5 태스크 관리 애플리케이션의 상태 관리/temp/src/store.js
+++ 7. Vuex를 이용한 데이터플로 설계 및 상태 관리/7.5 태스크 관리 애플리케이션의 상태 관리/temp/src/store.js
@@ -14,7 +14,30 @@ const store = createStore({
                 name: 'Vue.js 관련 책 사기',
                 done: true
             }
-        ]
+        ],
+        nextTaskId: 3,
+    },
+    mutations: {
+        addTask(state, {
+            name
+        }) {
+            state.tasks.push({
+                id: state.nextTaskId,
+                name,
+                done: false
+            })
+        },
+        toggleTaskStatus(state, {
+            id
+        }) {
+            const filtered = state.tasks.filter(task => {
+                return task.id === id
+            })
+
+            filtered.forEach(task => {
+                task.done = !task.done
+            })
+        }
     }
 })
 
