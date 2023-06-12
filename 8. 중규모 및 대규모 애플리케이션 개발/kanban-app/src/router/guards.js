ceffd26394eb5bb8cc8463045d37d352222485f8
ystone17 <ston5519@gmail.com>
2023-06-12 22:44:17 +0900

    라우팅 구현 - beforeEach 가드를 활용한 내비게이션 가드

---
+++ 8. 중규모 및 대규모 애플리케이션 개발/kanban-app/src/router/guards.js
@@ -0,0 +1,16 @@
+import store from '../store'
+
+export const authorizeToken = (to, from, next) => {
+  if (to.matched.some(recode => recode.meta.requiresAuth)) {
+    if (!store.state.auth || !store.state.auth.token) {
+      next({
+        path: '/login'
+      })
+      return
+    }
+
+    next()
+    return
+  }
+  next()
+}
--- 8. 중규모 및 대규모 애플리케이션 개발/kanban-app/src/store/index.js
+++ 8. 중규모 및 대규모 애플리케이션 개발/kanban-app/src/store/index.js
@@ -8,7 +8,7 @@ Vue.use(Vuex)
 
 const state = {
   auth: {
-    toenn: null,
+    token: null,
     userId: null
   },
   board: {
