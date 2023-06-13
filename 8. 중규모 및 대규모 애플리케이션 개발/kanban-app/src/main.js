82d99a94533578df527c886375f0b8657929a170
ystone17 <ston5519@gmail.com>
2023-06-13 22:21:11 +0900

    자식 컴포넌트 발생한 오류를 포착하는 간이 컴포넌트

--- 8. 중규모 및 대규모 애플리케이션 개발/kanban-app/src/main.js
+++ 8. 중규모 및 대규모 애플리케이션 개발/kanban-app/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import 'es6-promise/auto'
 import App from './App'
+import ErrorBoundary from './ErrorBoundary.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = process.env.NODE_EVN === 'production'
+
+Vue.component(ErrorBoundary.name, ErrorBoundary)
 
 /* eslint-disable no-new */
 new Vue({
